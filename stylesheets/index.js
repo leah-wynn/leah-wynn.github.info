de4ea94fc479c50831b3b8e0290a718a034df671
Leah Wynn <leahvwynn@gmail.com>
2017-03-02 22:10:41 -0500

    fixed page toggle

--- stylesheets/index.js
+++ stylesheets/index.js
@@ -1,28 +1,13 @@
 $(document).ready(function(){
-// $(".toggle_color").mouseover(function(){
-// 	$('body').append("<div id='toggleTool' style='position:fixed;'></div>");
-// 	$("#toggleTool").html("Click Me!");
-// 	   $('#toggleTool').css({
-//         "top" : $(this).offset().top + 1,
-//         "left" : $(this).offset().right
-//     });
-// });
-// $('.toggle_color').mouseleave(function(){
-//     $('#toggleTool').remove();
-// })
-
-
-	$('.toggle_color').click(function(e) {
-		 e.preventDefault();
-		$('html').removeClass().addClass('light');
-	})
-
-
-
-	// $('.toggle_color').click(function(e){
-	// 	e.preventDefault();
-	// 	$('html').removeClass().addClass('dark green');
-
-	// })
-
-})
+  $('.toggle_color').click(function () {
+    $('html').toggleClass(function () {
+      if ($(this).is('.dark')) {
+        $(this).removeClass();
+        return 'light green';
+      } else {
+        $(this).removeClass();
+        return 'dark green';
+      }
+    });
+  });
+});
