7a77d87b2713dc12cdf9bb6478d234c6034fb959
Leah Wynn <leahvwynn@gmail.com>
2017-03-02 21:20:29 -0500

    adding toggle button

--- stylesheets/index.js
+++ stylesheets/index.js
@@ -1,3 +1,28 @@
-$(".toggle_color").mouseover(function(){
-	
+$(document).ready(function(){
+// $(".toggle_color").mouseover(function(){
+// 	$('body').append("<div id='toggleTool' style='position:fixed;'></div>");
+// 	$("#toggleTool").html("Click Me!");
+// 	   $('#toggleTool').css({
+//         "top" : $(this).offset().top + 1,
+//         "left" : $(this).offset().right
+//     });
+// });
+// $('.toggle_color').mouseleave(function(){
+//     $('#toggleTool').remove();
+// })
+
+
+	$('.toggle_color').click(function(e) {
+		 e.preventDefault();
+		$('html').removeClass().addClass('light');
+	})
+
+
+
+	// $('.toggle_color').click(function(e){
+	// 	e.preventDefault();
+	// 	$('html').removeClass().addClass('dark green');
+
+	// })
+
 })
